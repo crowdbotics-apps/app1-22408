1278cc6b228e1cfec53c67d67c4eae6606569ce9
Crowdbotics <team@crowdbotics.com>
2020-11-08 20:18:51 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn2169523Navigator from '../features/SignIn2169523/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn2169523: { screen: SignIn2169523Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn2169523Reducer from '../features/SignIn2169523/redux/reducers'
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -15,5 +16,6 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn2169523: SignIn2169523Reducer,
 
 });
